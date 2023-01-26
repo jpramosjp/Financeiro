b359603b0af4c827b75c12e8d2107242c03e60e0
Joao Ramos <jpramosjp@hotmail.com>
2023-01-25 23:17:04 -0300

    configuracoes de servidor

--- financeiro/app/Providers/AppServiceProvider.php
+++ financeiro/app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if (isset($_SERVER['HTTPS']) && ($_SERVER['HTTPS'] == 'on' || $_SERVER['HTTPS'] == 1) || isset($_SERVER['HTTP_X_FORWARDED_PROTO']) &&  $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') {
+            $this->app['request']->server->set('HTTPS', true);
+        }
     }
 }
